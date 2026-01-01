4ce8c12cafee495372d71a7e4b4aa68bc8744549
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-01 21:07:51 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+from datetime import date
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
@@ -20,7 +21,32 @@ def init_db():
         user_id BIGINT PRIMARY KEY,
         username TEXT,
         points BIGINT DEFAULT 0,
+        last_daily DATE,
+        daily_streak INT DEFAULT 0,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
+    conn.commit()
+
+def add_user(user_id, username):
+    cur.execute("""
+        INSERT INTO users (user_id, username)
+        VALUES (%s, %s)
+        ON CONFLICT (user_id) DO NOTHING
+    """, (user_id, username))
+    conn.commit()
+
+def get_user(user_id):
+    cur.execute("SELECT points, last_daily, daily_streak FROM users WHERE user_id=%s", (user_id,))
+    return cur.fetchone()
+
+def update_daily(user_id, points, streak):
+    today = date.today()
+    cur.execute("""
+        UPDATE users
+        SET points = points + %s,
+            last_daily = %s,
+            daily_streak = %s
+        WHERE user_id = %s
+    """, (points, today, streak, user_id))
     conn.commit()
