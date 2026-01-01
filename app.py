1056ec195b0de2e40efd92c3dc96743a14b05dae
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-01 21:19:21 +0200

    Enhance index route with User-Agent check
    Added request validation for User-Agent header in index route.

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, abort
 import os
-from db import init_db
 
 app = Flask(__name__)
 
-try:
-    init_db()
-except Exception as e:
-    print("DB ERROR:", e)
-
 @app.route("/")
-def home():
+def index():
+    # التأكد إن الفتح من Telegram
+    if not request.headers.get("User-Agent"):
+        abort(403)
     return send_file("index.html")
 
 if __name__ == "__main__":
