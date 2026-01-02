fdffbe6320b32ed3f973d8a0e78e862aac58d8f5
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-02 17:54:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return send_file("index.html")
+def index():
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
