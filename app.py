768974c6e10f6428c3f1be206cda6ac4cd620cef
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-02 07:43:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, send_file, request, abort
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # التأكد إن الفتح من Telegram
-    if not request.headers.get("User-Agent"):
-        abort(403)
-    return send_file("index.html")
+    return send_from_directory(".", "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(".", path)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
