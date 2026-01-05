33182e0f4445e58c1f1bef2c617bb7f85cec5a80
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-05 19:48:56 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const BOT_TOKEN = "8440388547:AAF8Ftiu7Qmi1GLfHzViIRAzgZE7khvvXC8";
+const WEB_APP_URL = "https://ggerscoin-production.up.railway.app/";
+
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "🐝 مرحبًا بك في WinHive\n\nاضغط على الزر للدخول إلى التطبيق 👇",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "🐝 دخول تطبيق WinHive",
+              web_app: { url: WEB_APP_URL }
+            }
+          ]
+        ]
+      }
+    }
+  );
+});
+
+console.log("WinHive Bot Running...");
