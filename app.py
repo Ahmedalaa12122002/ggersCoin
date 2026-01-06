91626782c156534cfa9ad9d94019e06179383f65
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 19:53:47 +0200

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import telebot
 
-BOT_TOKEN = "8088771179:AAHE_OhI7Hgq1sXZfHCdYtHd2prBvHzg_rQ"
-WEB_APP_URL = "https://YOUR_PROJECT.up.railway.app/"
+BOT_TOKEN = os.environ.get("8088771179:AAHE_OhI7Hgq1sXZfHCdYtHd2prBvHzg_rQ")
+APP_URL = os.environ.get("https://ggerscoin-production.up.railway.app/")  # https://xxxx.up.railway.app
 
 bot = telebot.TeleBot(BOT_TOKEN)
 app = FastAPI()
 
 # ===== Telegram Webhook =====
 @app.post("/webhook")
-async def telegram_webhook(request: Request):
-    data = await request.json()
-    update = telebot.types.Update.de_json(data)
+async def telegram_webhook(req: Request):
+    update = telebot.types.Update.de_json(await req.json())
     bot.process_new_updates([update])
     return {"ok": True}
 
 @bot.message_handler(commands=["start"])
-def start_handler(message):
+def start(message):
     kb = telebot.types.InlineKeyboardMarkup()
     kb.add(
         telebot.types.InlineKeyboardButton(
             "🚀 دخول التطبيق",
-            web_app=telebot.types.WebAppInfo(url=WEB_APP_URL)
+            web_app=telebot.types.WebAppInfo(url=APP_URL)
         )
     )
     bot.send_message(
         message.chat.id,
-        "👋 أهلاً بك\n\nاضغط الزر للدخول إلى التطبيق",
+        "👋 أهلاً بك\nاضغط الزر للدخول",
         reply_markup=kb
     )
 
 @app.on_event("startup")
 async def startup():
     bot.remove_webhook()
-    bot.set_webhook(url=WEB_APP_URL + "webhook")
+    bot.set_webhook(url=f"{APP_URL}/webhook")
 
 # ===== Web App =====
 app.mount("/static", StaticFiles(directory="webapp"), name="static")
