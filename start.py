a7a0f233631af49ba444b996293b0cd1cc5d9f16
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 16:50:36 +0200

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ def start_message(bot, message):
     bot.send_message(
         chat_id=message.chat.id,
         text=(
-            "👋 أهلاً بك في GgersCoin\n\n"
+            "👋 أهلاً بك في Pram points\n\n"
             "🚀 اضغط على الزر بالأسفل للدخول إلى التطبيق"
         ),
         reply_markup=start_keyboard()
