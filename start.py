4a8a53be530b089fc4c8bacc0ceab3b3084d553b
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 16:14:23 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,11 @@
-# bot/start.py
-
 from keyboards import start_keyboard
 
 def start_message(bot, message):
-    text = (
-        "👋 أهلاً بك في التطبيق\n\n"
-        "💡 من هنا يمكنك إدارة كل شيء بسهولة\n"
-        "👇 اضغط على الزر للدخول إلى التطبيق"
-    )
     bot.send_message(
         chat_id=message.chat.id,
-        text=text,
+        text=(
+            "👋 أهلاً بك في التطبيق\n\n"
+            "🚀 اضغط على الزر بالأسفل للدخول إلى Web App"
+        ),
         reply_markup=start_keyboard()
     )
