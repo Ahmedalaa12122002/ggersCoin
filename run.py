f601ecd988c9cc63712ecf4006398be1bd4efc31
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 05:53:25 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ from bot.main import setup_handlers
 BOT_TOKEN = "8440388547:AAF8Ftiu7Qmi1GLfHzViIRAzgZE7khvvXC8"
 
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    setup_handlers(app)
-    app.run_polling()
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    setup_handlers(application)
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
