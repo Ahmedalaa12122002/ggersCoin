3c572670f2522f88b676f3ee32d59450516efe50
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 18:34:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
 import telebot
-from start import start_message
 
 BOT_TOKEN = "8088771179:AAGvjDfgYc8LbwMhCyO1cgR-5zPdqYllhwE"
+WEB_APP_URL = "https://ggerscoin-production.up.railway.app/"
 
 bot = telebot.TeleBot(BOT_TOKEN)
-
-# مهم جدًا لمنع أي تعارض قديم
 bot.delete_webhook(drop_pending_updates=True)
 
 @bot.message_handler(commands=["start"])
-def handle_start(message):
-    start_message(bot, message)
+def start(message):
+    kb = telebot.types.InlineKeyboardMarkup()
+    kb.add(
+        telebot.types.InlineKeyboardButton(
+            "🚀 دخول التطبيق",
+            web_app=telebot.types.WebAppInfo(url=WEB_APP_URL)
+        )
+    )
 
-print("BOT STARTED SUCCESSFULLY")
-bot.infinity_polling(skip_pending=True)
+    bot.send_message(
+        message.chat.id,
+        "👋 أهلاً بك\n\nاضغط على الزر للدخول إلى التطبيق",
+        reply_markup=kb
+    )
+
+print("BOT RUNNING")
+bot.infinity_polling()
