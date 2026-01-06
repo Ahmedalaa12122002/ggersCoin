0a87b804a044ced41fefca8a951bcd40c7d6eb27
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 16:15:13 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,14 @@
-# bot/keyboards.py
-
 from telebot import types
-from config import WEB_APP_URL
+import os
+
+WEB_APP_URL = os.getenv("WEB_APP_URL")
 
 def start_keyboard():
-    keyboard = types.InlineKeyboardMarkup()
-    web_app_btn = types.InlineKeyboardButton(
-        text="🚀 دخول التطبيق",
-        web_app=types.WebAppInfo(url=WEB_APP_URL)
+    note = types.InlineKeyboardMarkup()
+    note.add(
+        types.InlineKeyboardButton(
+            text="🚀 دخول التطبيق",
+            web_app=types.WebAppInfo(url=WEB_APP_URL)
+        )
     )
-    keyboard.add(web_app_btn)
-    return keyboard
+    return note
