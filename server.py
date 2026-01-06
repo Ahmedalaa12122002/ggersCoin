117c10cf1826335527613adc88c304aa1da6f62b
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-06 18:55:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
+# static files
 app.mount("/static", StaticFiles(directory="webapp"), name="static")
 
 @app.get("/")
-def root():
+def home():
     return FileResponse("webapp/index.html")
+
+# مهم لـ Railway
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
