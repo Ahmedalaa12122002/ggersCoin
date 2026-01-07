e4b9907d0e6b639cdd8e2348b159b49066812f23
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-07 15:53:21 +0200

    Create auth.js

---
+++ webapp/auth.js
@@ -0,0 +1,20 @@
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+
+const user = tg.initDataUnsafe?.user;
+
+if (user) {
+  const authUser = {
+    id: user.id,
+    first_name: user.first_name || "",
+    last_name: user.last_name || "",
+    username: user.username || "",
+    language: user.language_code || "en"
+  };
+
+  localStorage.setItem("tg_user", JSON.stringify(authUser));
+  console.log("Telegram User:", authUser);
+} else {
+  console.warn("Telegram user not found");
+}
