d0ffc15e268f897cdc8ad521411c25dc60d71c2b
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-07 07:15:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"status": "OK", "message": "Web is working"}
+def home():
+    return FileResponse("webapp/index.html")
