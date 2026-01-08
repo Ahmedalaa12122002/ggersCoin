194d9f6f2c5a9ad080c7f3a45af2a0f131077225
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-08 08:40:48 +0200

    Update lands.py

--- api/farm/lands.py
+++ api/farm/lands.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/api/farm",
+    tags=["Farm"]
+)
 
-@router.get("/api/farm/lands")
+@router.get("/lands")
 def get_lands():
+    lands = []
+
+    for i in range(1, 13):
+        lands.append({
+            "id": i,
+            "unlocked": True if i == 1 else False,
+            "vip_required": False if i == 1 else True,
+            "status": "empty"
+        })
+
     return {
-        "lands": [
-            {"id": 1, "locked": False},
-            {"id": 2, "locked": True},
-            {"id": 3, "locked": True},
-            {"id": 4, "locked": True},
-            {"id": 5, "locked": True},
-            {"id": 6, "locked": True},
-            {"id": 7, "locked": True},
-            {"id": 8, "locked": True},
-            {"id": 9, "locked": True},
-            {"id": 10, "locked": True},
-            {"id": 11, "locked": True},
-            {"id": 12, "locked": True}
-        ]
+        "success": True,
+        "lands": lands
     }
