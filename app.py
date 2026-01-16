9af1ccec2d81b95776472602c8011122705bc11e
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-16 07:44:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ app.mount("/static", StaticFiles(directory="webapp"), name="static")
 @app.get("/")
 def home():
     return HTMLResponse("""
-    <h1>✅ الويب شغال</h1>
+    <h1>✅ الويب مرحبا شغال</h1>
     <p>تم تشغيل Web App بنجاح</p>
     """)
