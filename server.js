a35ec93f2419ce2295f80e13016c95682f98bc2f
Ahmedalaa12122002 <ahmedpop25ahmed@gmail.com>
2026-01-16 19:26:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log("✅ Web server running on port", PORT);
+  console.log("✅ Server running on port", PORT);
 });
